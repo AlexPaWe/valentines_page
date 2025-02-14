c303075714b151bd0c2a0bfb2dbcc42eae381fe1
AlexPaWe <alex-w93@gmx.de>
2025-02-14 18:12:30 +0100

    Added GNU GPLv3 license to the project along with a notice on each source file.

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+/* Valentines Page: This is a webpage to offer digital goods (pdf) as a interactive letter in the style of ps1 games.
+Copyright (C) 2025  AlexPaWe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 import * as THREE from 'three';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPixelatedPass } from 'three/addons/postprocessing/RenderPixelatedPass.js';
